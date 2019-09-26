631a22bc8cbc83c7000bd163fadab252e7be343e
Jack Kawell <jgkawell@outlook.com>
2019-09-26 15:13:39 -0600

    Created module for getting column of data from stdin

--- get_data.py
+++ get_data.py
@@ -1,3 +1,18 @@
+import sys
+
 
 def read_stdin_col(col_num):
-    return None
+    stdin_list = []
+    for el in sys.stdin:
+        stdin_list.append(int(el.split()[col_num]))
+
+    return stdin_list
+
+
+def main():
+    stdin_list = read_stdin_col(0)
+    print(stdin_list)
+
+
+if __name__ == "__main__":
+    main()
