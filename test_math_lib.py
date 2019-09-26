8e8ac20999498d375bee3666aabd02767cda6165
Jack Kawell <jgkawell@outlook.com>
2019-09-26 13:40:31 -0600

    Added simple test for passing empty list

--- test_math_lib.py
+++ test_math_lib.py
@@ -4,9 +4,14 @@ import math_lib
 
 class TestMathLib(unittest.TestCase):
 
-    def test_list_mean_empty_list(self):
+    def test_list_mean_none_list(self):
         result = math_lib.list_mean(None)
         self.assertEqual(result, None)
+        
+    def test_list_mean_empty_list(self):
+        result = math_lib.list_mean([])
+        self.assertEqual(result, None)
+
 
 
 if __name__ == '__main__':
