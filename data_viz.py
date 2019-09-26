a748929f22ba18f57040b3e1d8e3054a45ae6f53
Jack Kawell <jgkawell@outlook.com>
2019-09-26 17:09:33 -0600

    Added in combo code

--- data_viz.py
+++ data_viz.py
@@ -37,4 +37,23 @@ def histogram(L, out_file_name):
 
 
 def combo(L, out_file_name):
-    pass
+
+    if os.path.exists(out_file_name):
+        raise FileExistsError("That file name already exists.")
+
+    mean = ml.list_mean(L)
+    stdev = ml.list_stdev(L)
+    plt.title("mean: " + str(mean) + " stdev: " + str(stdev))
+
+    plt.subplot(1, 2, 1)
+    plt.boxplot(L)
+    plt.xlabel("Column Number")
+    plt.ylabel("Value")
+
+    plt.subplot(1, 2, 2)
+    plt.hist(L)
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+
+    plt.savefig(out_file_name)
+    plt.close()
