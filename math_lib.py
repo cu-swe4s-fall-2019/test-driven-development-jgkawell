210427b0e517cb77fc889ce4d826ff1cba4c9519
Jack Kawell <jgkawell@outlook.com>
2019-09-26 13:52:24 -0600

    Fixed lib behavior with new test

--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,14 @@
 
 def list_mean(L):
-    return None
+
+    if L is None:
+        return None
+
+    if len(L) == 0:
+        return None
+
+    return sum(L) / len(L)
+
 
 def list_stdev(L):
     return None
