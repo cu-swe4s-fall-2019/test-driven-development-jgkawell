f1e421b4b756eb22a71b280cc3a40801799147bb
Jack Kawell <jgkawell@outlook.com>
2019-09-26 17:08:59 -0600

    Added tests for combo

--- test_data_viz.py
+++ test_data_viz.py
@@ -66,6 +66,36 @@ class TestMathLib(unittest.TestCase):
 
         os.remove(file_name)
 
+    def test_combo_file_already_exists(self):
+        file_name = "test.png"
+        f = open(file_name, "w+")
+        f.close()
+
+        with self.assertRaises(FileExistsError) as ex:
+            data_viz.combo([], file_name)
+
+        self.assertEqual(str(ex.exception), "That file name already exists.")
+
+        os.remove(file_name)
+
+    def test_combo_random_int_array(self):
+        file_name = "test.png"
+        rand_int_list = [random.randrange(-1000, 1000) for i in range(1000)]
+        data_viz.combo(rand_int_list, file_name)
+
+        self.assertEqual(os.path.exists(file_name), True)
+
+        os.remove(file_name)
+
+    def test_combo_random_double_array(self):
+        file_name = "test.png"
+        rand_double_list = [random.random() for i in range(1000)]
+        data_viz.combo(rand_double_list, file_name)
+
+        self.assertEqual(os.path.exists(file_name), True)
+
+        os.remove(file_name)
+
 
 if __name__ == '__main__':
     unittest.main()
