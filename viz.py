45836ea3fece990be02ad103029e6879b249dd0b
Jack Kawell <jgkawell@outlook.com>
2019-09-26 17:40:16 -0600

    Added the viz module for user facing data visualization

--- viz.py
+++ viz.py
@@ -1,6 +1,32 @@
+import get_data
+import data_viz
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Plot a data visualization from standard in (stdin).')
+parser.add_argument(
+    '--out_file',
+    type=str,
+    help='The file name of the plot to be generated')
+parser.add_argument(
+    '--plot_type',
+    type=str,
+    help='The type of plot to be generated (boxplot, histogram, or combo)')
+
 
 def main():
-    pass
+    args = parser.parse_args()
+    data = get_data.read_stdin_col(1)
+
+    if args.plot_type == "boxplot":
+        data_viz.boxplot(data, args.out_file)
+    elif args.plot_type == "histogram":
+        data_viz.histogram(data, args.out_file)
+    elif args.plot_type == "combo":
+        data_viz.combo(data, args.out_file)
+    else:
+        print("Only the following plot types are allowed: "
+              + "boxplot, histogram, or combo")
 
 
 if __name__ == '__main__':
