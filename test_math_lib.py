e74eb84aa78c521e45c1a451abd4b3b477947b55
Jack Kawell <jgkawell@outlook.com>
2019-09-26 13:49:09 -0600

    Added random int test

--- test_math_lib.py
+++ test_math_lib.py
@@ -1,5 +1,8 @@
 import unittest
 import math_lib
+import random
+import math
+import statistics
 
 
 class TestMathLib(unittest.TestCase):
@@ -11,6 +14,11 @@ class TestMathLib(unittest.TestCase):
     def test_list_mean_empty_list(self):
         result = math_lib.list_mean([])
         self.assertEqual(result, None)
+
+    def test_list_mean_int_list(self):
+        rand_list = [random.randrange(-1000, 1000) for i in range(1000)]
+        result = math_lib.list_mean(rand_list)
+        self.assertEqual(result, statistics.mean(rand_list))
 
 
 if __name__ == '__main__':
